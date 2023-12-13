17f015bc93cc1a87901e0aa4b75c794f7c4e1f35
Liden <lninyow@gmail.com>
2023-12-13 21:30:29 +0800

    events screen

--- src/routes/Events.tsx
+++ src/routes/Events.tsx
@@ -1,3 +1,38 @@
+import { Button } from "@nextui-org/button";
+import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
+import { Divider } from "@nextui-org/divider";
+
 export default function Events() {
-  return <div>Events</div>;
+  function EventDetails() {
+    return (
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <div className="flex flex-col">
+            <p className="text-2xl font-bold">EVENT NAME</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <div className="flex items-center justify-center gap-8 p-8">
+            <p>START DATE</p>
+            <p>END DATE</p>
+          </div>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Button>View Participants</Button>
+        </CardFooter>
+      </Card>
+    );
+  }
+
+  const events = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+  return (
+    <div className="flex flex-wrap justify-center gap-8 p-8">
+      {events.map((index) => (
+        <EventDetails key={index} />
+      ))}
+    </div>
+  );
 }
