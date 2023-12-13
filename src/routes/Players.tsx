d52bddbd784bdd2f0d114cc19a50a579dd901a4d
Liden <lninyow@gmail.com>
2023-12-13 20:44:06 +0800

    added players table

--- src/routes/Players.tsx
+++ src/routes/Players.tsx
@@ -1,3 +1,66 @@
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
+
 export default function Players() {
-  return <div>Players</div>;
+  const columns = [
+    { key: "name", header: "Player Name" },
+    { key: "wins", header: "Number of Wins" },
+    { key: "gender", header: "Gender" },
+    { key: "actions", header: "Actions" },
+  ];
+
+  const data: any[] = [
+    { name: "Player 1", wins: 10, gender: "Male" },
+    { name: "Player 2", wins: 20, gender: "Female" },
+    { name: "Player 3", wins: 30, gender: "Female" },
+  ];
+
+  return (
+    <div className="flex items-center p-8">
+      <Table>
+        <TableHeader>
+          {columns.map((column) => (
+            <TableColumn key={column.key}>{column.header}</TableColumn>
+          ))}
+        </TableHeader>
+        <TableBody emptyContent={"No rows to display."}>
+          {data.map((row, rowIndex) => (
+            <TableRow key={rowIndex}>
+              {columns.map((column) => (
+                <TableCell key={column.key}>
+                  {column.key === "actions" ? (
+                    <Dropdown>
+                      <DropdownTrigger>
+                        <Button variant="bordered">Actions</Button>
+                      </DropdownTrigger>
+                      <DropdownMenu aria-label="Dynamic Actions">
+                        <DropdownItem key="edit">Edit</DropdownItem>
+                        <DropdownItem key="view">View</DropdownItem>
+                        <DropdownItem key="delete" color="danger" className="text-danger">
+                          Delete
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </Dropdown>
+                  ) : (
+                    row[column.key]
+                  )}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
