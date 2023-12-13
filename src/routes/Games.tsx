c7586eb645e89ab05f32b029470d0966e25ea352
Liden <lninyow@gmail.com>
2023-12-13 21:39:49 +0800

    game screen

--- src/routes/Games.tsx
+++ src/routes/Games.tsx
@@ -1,3 +1,34 @@
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider } from "@nextui-org/react";
+
 export default function Games() {
-  return <div>Games</div>;
+  function GameDetails() {
+    return (
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <div className="flex flex-col">
+            <p className="text-2xl font-bold">GAME NAME</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <div className="flex items-center justify-center gap-8 p-8">
+            <p>PINA KA CHUY NGA DESCRIPTION AMASDUIJASUIDSAUIDNSAUIDNSIADU</p>
+          </div>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Button>View Participants</Button>
+        </CardFooter>
+      </Card>
+    );
+  }
+  const games = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+  return (
+    <div className="flex flex-wrap justify-center gap-8 p-8">
+      {games.map((index) => (
+        <GameDetails key={index} />
+      ))}
+    </div>
+  );
 }
