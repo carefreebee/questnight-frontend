832b430195f11f401388872375efe6e2ad7713d4
Nathaniel Tampus <nate@mugnavo.com>
2023-12-13 19:58:47 +0800

    add react-router & initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
+import Events from "~/routes/Events";
+import Games from "~/routes/Games";
+import Home from "~/routes/Home";
+import Players from "~/routes/Players";
+
 function App() {
   return (
-    <div className="flex p-8">
-      <button className="rounded-md bg-blue-400 p-2 transition-colors hover:bg-blue-200">
-        hello, tailwind
-      </button>
-    </div>
+    <>
+      {/* TODO: some shared components here on top, like a header/navbar? or footer */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<Games />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/players" element={<Players />} />
+      </Routes>
+    </>
   );
 }
 
