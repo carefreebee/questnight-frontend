a8940d0723b6e1feb8267c772bf793c45cb364e7
Nathaniel Tampus <nate@mugnavo.com>
2023-12-13 19:49:56 +0800

    add tailwind & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
-import { useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React GWAPO KO</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="flex p-8">
+      <button className="rounded-md bg-blue-400 p-2 transition-colors hover:bg-blue-200">
+        hello, tailwind
+      </button>
+    </div>
   );
 }
 
